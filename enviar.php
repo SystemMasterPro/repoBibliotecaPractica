32a7cc3b74f1fdaea719b24cffca76fce589180c
Richard <richardjimenez.9641@gmail.com>
2020-10-21 15:54:36 -0500

    Mejorando envio

--- enviar.php
+++ enviar.php
@@ -1,12 +1,9 @@
 <?php
 //En el destino colocar el correo alque quieres que lleguen los datos del contacto de tu formulario
-    $destino = "richardjimenez.9641@gmail.com";
-    $name = $_POST["name"];
-    $mail = $_POST["mail"];
-    $phone = $_POST["phone"];
-    $message = $_POST["message"];
-    $contenido = "Nombre: " . $name . "\nCorreo: " . $mail . "\nNúmero: " . $phone . "\nAsunto: " . $message;
-    mail($destino, "Contacto", $contenido);
-    header("Location:index.html");
+$to = "richardjimenez.9641@gmail.com";
+$subject = "RAPIDS";
+$message = "Este es mi primer envío de email con PHP";
+$headers = "From: mi@cuentadeemail.com" . "\r\n" . "CC: destinatarioencopia@email.com";
+mail($to, $subject, $message, $headers);
 //Esto es opcional, aqui pueden colocar un mensaje de "enviado correctamente" o redireccionarlo a algun lugar
 ?>
