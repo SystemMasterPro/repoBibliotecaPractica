a3b9d8b0b4ffb605033da7e52a2154e1d844ec7a
Richard <richardjimenez.9641@gmail.com>
2020-10-21 16:16:34 -0500

    Ultimas mejoras

--- enviar.php
+++ enviar.php
@@ -1,9 +1,27 @@
 <?php
 //En el destino colocar el correo alque quieres que lleguen los datos del contacto de tu formulario
-$to = "richardjimenez.9641@gmail.com";
-$subject = "RAPIDS";
-$message = "Este es mi primer envío de email con PHP";
-$headers = "From: mi@cuentadeemail.com" . "\r\n" . "CC: destinatarioencopia@email.com";
-mail($to, $subject, $message, $headers);
+<?php
+$nombre = $_POST['name'];
+$mail = $_POST['mail'];
+$empresa = $_POST['message'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['message'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time()) . " \r\n";
+$mensaje .= "Y su numero es el ". $_POST['phone'];
+
+$para = 'ignaciobluuweb@gmail.com';
+$asunto = 'RAPIDS';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:index.html");
+?>
 //Esto es opcional, aqui pueden colocar un mensaje de "enviado correctamente" o redireccionarlo a algun lugar
 ?>
